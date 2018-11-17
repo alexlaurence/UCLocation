95f6188e6464e8a497e9d5e063df9304046b34f2
Alexander Laurence <alexander.adamlaurence@gmail.com>
2018-11-17 16:02:15 +0000

    First commit

---
+++ Unity/Assets/UCLFinder.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_ANDROID
+namespace AndroidGoodiesExamples
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using DeadMosquito.AndroidGoodies;
+
+
+    public class UCLFinder : MonoBehaviour
+    {
+        [SerializeField]
+        private Text placeholder;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            placeholder.text = "BSSID: " + AGNetwork.GetWifiConnectionInfo();
+        }
+    }
+}
+#endif
