38c67dba0e92332c3359add7f4b36c8cb33eae33
Alexander Laurence <39573687+alexlaurence@users.noreply.github.com>
2018-11-18 01:35:54 +0000

    Update GoogleApi.cs

--- Unity/Assets/GoogleApi.cs
+++ Unity/Assets/GoogleApi.cs
@@ -32,7 +32,7 @@
             url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon +
                 "&zoom=" + zoom + "&size=" + mapWidth + "x" + mapHeight + "&scale=" + scale
                 + "&maptype=" + mapSelected +
-                "&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyACn3s2FTYulPltpCwAmbEJfAsdwj9IlEo";
+                "&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=";
             WWW www = new WWW(url);
             yield return www;
             img.texture = www.texture;
